acca80e091d9fffaedbd708dbce0e306a1a617c2
Delali Kanda <dkanda@nthc.com.gh>
2016-05-31 12:19:23 +0000

    changed method of getting RAM

--- app/Utilities/MemoryUtils.php
+++ app/Utilities/MemoryUtils.php
@@ -16,20 +16,28 @@ class MemoryUtils
     {
         $memoryInfoData = [];
 
-        $data = explode("\n", file_get_contents("/proc/meminfo"));
+        // $data = explode("\n", file_get_contents("/proc/meminfo"));
+        //
+        // $meminfo = array();
+        //
+        // foreach ($data as $line) {
+        //     list($key, $val) = explode(":", $line);
+        //     $meminfo[$key] = trim(str_replace("kB", "", $val));
+        // }
 
-        $meminfo = array();
+        $free = shell_exec('free');
+    	$free = (string)trim($free);
+    	$free_arr = explode("\n", $free);
+    	$mem = explode(" ", $free_arr[1]);
+    	$mem = array_filter($mem);
+    	$mem = array_merge($mem);
+    	$memory_usage = $mem[2]/$mem[1]*100;
 
-        foreach ($data as $line) {
-            list($key, $val) = explode(":", $line);
-            $meminfo[$key] = trim(str_replace("kB", "", $val));
-        }
-
-        $memoryInfoData['totalMemory'] = ['disp' => self::formatKBToGBDisp($meminfo[self::$totalMemIndex]), 'raw' => self::formatKBToGB($meminfo[self::$totalMemIndex])];
-        $memoryInfoData['availableMemory'] = ['disp' => self::formatKBToGBDisp($meminfo[self::$availableMemIndex]) ,'raw' => self::formatKBToGB($meminfo[self::$availableMemIndex])];
-        $memoryInfoData['usedMemory'] = [
-            'disp' => self::formatKBToGBDisp($meminfo[self::$totalMemIndex] - $meminfo[self::$availableMemIndex]),
-            'raw'  => self::formatKBToGB($meminfo[self::$totalMemIndex] - $meminfo[self::$availableMemIndex]),
+        $memoryInfoData['totalMemory'] = ['disp' => self::formatKBToGBDisp($mem[1]), 'raw' => self::formatKBToGB($mem[1])];
+        $memoryInfoData['usedMemory'] = ['disp' => self::formatKBToGBDisp($mem[2]) ,'raw' => self::formatKBToGB($mem[2])];
+        $memoryInfoData['availableMemory'] = [
+            'disp' => self::formatKBToGBDisp($mem[1] - $mem[2]),
+            'raw'  => self::formatKBToGB($mem[1] - $mem[2]),
         ];
 
         return $memoryInfoData;
@@ -42,7 +50,7 @@ class MemoryUtils
 
     public function formatKBToGBDisp($kilobyteData)
     {
-        return $kilobyteData / 1000000 . " GB";
+        return number_format(($kilobyteData / 1000000),2) . " GB";
     }
 
 }
